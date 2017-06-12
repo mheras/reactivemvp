73ecda53fdbee92c8ad48ad2e387d7b620a55492
Martin Heras <martin.heras@mercadolibre.com>
2017-06-12 01:28:49 -0300

    Added lib module.

---
+++ lib/src/androidTest/java/com/mheras/reactivemvp/ExampleInstrumentedTest.kt
@@ -0,0 +1,25 @@
+package com.mheras.reactivemvp
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.lang.Exception
+
+/**
+ * Instrumented test, which will execute on an Android device.
+
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("com.mheras.reactivemvp.test", appContext.packageName)
+    }
+}
